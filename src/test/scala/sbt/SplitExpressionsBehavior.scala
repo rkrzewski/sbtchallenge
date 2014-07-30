812911b08464b11d7d4358501191fadd1d5c6ba8
Rafał Krzewski <rafal.krzewski@caltha.pl>
2014-07-30 19:33:01 +0200

    tests for vals and lazy vals

--- src/test/scala/sbt/SplitExpressionsBehavior.scala
+++ src/test/scala/sbt/SplitExpressionsBehavior.scala
@@ -3,19 +3,19 @@ package sbt
 import org.scalatest.FlatSpec
 
 trait SplitExpressionsBehavior { this: FlatSpec =>
-  
+
   def split(s: String)(implicit splitter: SplitExpressions) = splitter.splitExpressions(s.split("\n").toSeq)
-  
+
   def oldExpressionsSplitter(implicit splitter: SplitExpressions) {
 
     it should "parse a simple setting" in {
       val (imports, settingsAndDefs) = split("""version := "1.0"""")
       assert(settingsAndDefs.head._1 === """version := "1.0"""")
-      
+
       assert(imports.isEmpty)
       assert(!settingsAndDefs.isEmpty)
     }
-    
+
     it should "parse a config containing a single import" in {
       val (imports, settingsAndDefs) = split("""import foo.Bar""")
       assert(!imports.isEmpty)
@@ -30,18 +30,30 @@ trait SplitExpressionsBehavior { this: FlatSpec =>
       assert(!settingsAndDefs.isEmpty)
     }
 
+    it should "parse a config containgn a val" in {
+      val (imports, settingsAndDefs) = split("""val answer = 42""")
+      assert(imports.isEmpty)
+      assert(!settingsAndDefs.isEmpty)
+    }
+
+    it should "parse a config containgn a lazy val" in {
+      val (imports, settingsAndDefs) = split("""lazy val root = (project in file(".")).enablePlugins­(PlayScala)""")
+      assert(imports.isEmpty)
+      assert(!settingsAndDefs.isEmpty)
+    }
+
   }
-  
+
   def newExpressionsSplitter(implicit splitter: SplitExpressions) {
-    
+
     it should "parse a two settings without intervening blank line" in {
       val (imports, settings) = split("""version := "1.0"
 scalaVersion := "2.10.4"""")
-      
+
       assert(imports.isEmpty)
       assert(settings.size === 2)
     }
-    
+
   }
-  
+
 }
