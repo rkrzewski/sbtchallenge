2093a5295b8ed4636fbbf02ce3eb725388a13e81
ajozwik <jozwik@onet.pl>
2014-08-21 11:31:26 +0200

    Formatting corrected - only changes

--- src/main/scala/sbt/EvaluateConfigurationsScalania.scala
+++ src/main/scala/sbt/EvaluateConfigurationsScalania.scala
@@ -1,21 +1,17 @@
 package sbt
 
-import scala.tools.reflect.ToolBoxError
-
-
 class EvaluateConfigurationsScalania extends SplitExpressions {
-
-
-  def splitExpressions(lines: Seq[String]): (Seq[(String, Int)], Seq[(String, LineRange)]) = {
+  def splitExpressions(lines: Seq[String]): (Seq[(String, Int)], Seq[(String, LineRange)]) =
+  {
     import scala.reflect.runtime._
     import scala.reflect.runtime.universe._
     import scala.tools.reflect.ToolBox
+    import scala.tools.reflect.ToolBoxError
     import scala.compat.Platform.EOL
+
     val mirror = universe.runtimeMirror(this.getClass.getClassLoader)
     val toolbox = mirror.mkToolBox(options = "-Yrangepos")
     val merged = lines.mkString("\n")
-    // block try-catch should be added in private[sbt] def evaluateSbtFile.
-    // Here we do not have information about input file
     val parsed =
       try {
         toolbox.parse(merged)
@@ -33,15 +29,15 @@ class EvaluateConfigurationsScalania extends SplitExpressions {
         Seq(t)
     }
 
-    val (imports, statements) = parsedTrees partition (_ match {
+    val (imports, statements) = parsedTrees partition {
       case _: Import => true
       case _ => false
-    })
+    }
 
     def convertImport(t: Tree): (String, Int) =
-      (merged.substring(t.pos.start, t.pos.end), t.pos.line)
+      (merged.substring(t.pos.start, t.pos.end), t.pos.start)
     def convertStatement(t: Tree): (String, LineRange) =
-      (merged.substring(t.pos.start, t.pos.end), LineRange(t.pos.line, t.pos.end))
+      (merged.substring(t.pos.start, t.pos.end), LineRange(t.pos.start, t.pos.end))
 
     (imports map convertImport, statements map convertStatement)
   }
